dac673ccdc17aa42c734454651be7076744b78d3
Sebastian Müller <senpo@posteo.de>
2014-05-13 16:35:14 +0200

    fixed integer errors uint()

--- RCS_Estimation.m
+++ RCS_Estimation.m
@@ -24,11 +24,11 @@ sigma = 5;  % radar cross section [m^2]
 
 % radar constants
 
-f_a = 100e3; % sampling frequency [Hz]
+f_a = 200e4; % sampling frequency [Hz]
 
 f_0 = 2.4e9;   % center frequency [Hz]
 B = 100e3;     % sweep frequency [Hz]
-T_f = 3e-4;  % sweep time per flank [s]
+T_f = 1e-3;  % sweep time per flank [s]
 n = 5;      % frequency steps per flank [1]
 N = 1;      % measuring intervals [1]
 
@@ -37,7 +37,8 @@ P_s = 100;  % transmission power [W]
 % calculate remaining data
 
 T = N*2*T_f; % calculate measure time
-t = linspace(0, T, f_a*T); % create time vector
+S = uint64(f_a*T);   % number of samples
+t = linspace(0, T, S); % create time vector
 
 f_smcw = SMCW_Freq(t, 2*T_f, f_0, B, n, N); % create frequency vector
 
@@ -48,12 +49,12 @@ f_D = 2*v*f_0/c;    % doppler frequency
 
 % transmitted signal
 
-s = sqrt(P_s) * exp(1i * 2 * pi * discrete_int(f_smcw, 1/f_a, f_a*T));
+s = sqrt(P_s) * exp(1i * 2 * pi * discrete_int(f_smcw, 1/f_a, S));
 
 % received signal
 
 r = 0.5 * sqrt(P_s) * exp(1i * 2 * pi * discrete_int(f_smcw, 1/f_a, ceil(f_a*(T-tau))));
-r = [zeros(1, f_a*T - ceil(f_a*(T-tau))) r]; % fill received vector with nulls for same length
+r = [zeros(1, uint32(f_a*T - ceil(f_a*(T-tau)))) r]; % fill received vector with nulls for same length
 r = r .* exp(1i * 2 * pi * f_D * t); % add phase shift because of doppler
 
 
