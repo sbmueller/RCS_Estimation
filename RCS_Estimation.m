14d80c62995bbb3d168f6407f9bf45d62fc8b547
Sebastian Müller <senpo@posteo.de>
2014-05-26 16:44:35 +0200

    Temp commit

--- RCS_Estimation.m
+++ RCS_Estimation.m
@@ -18,7 +18,7 @@ c = 299792458; % speed of light [m/s]
 
 % object constants
 
-R = 100;     % distance radar - target [m]
+R = 10;     % distance radar - target [m]
 v = 0;      % speed of target [m/s] (v>0 -> moves towards receiver)
 sigma = 5;  % radar cross section [m^2]
 
@@ -27,9 +27,9 @@ sigma = 5;  % radar cross section [m^2]
 f_a = 100e3; % sampling frequency [Hz]
 
 f_0 = 24.125e9;   % center frequency [Hz]
-B = 5e6;     % sweep frequency [Hz]
+B = 15e6;     % sweep frequency [Hz]
 T_f = 10e-3;  % sweep time per flank [s]
-n = 128;      % frequency steps per flank [1]
+n = 32;      % frequency steps per flank [1]
 N = 1;      % measuring intervals [1]
 
 P_s = 100;  % transmission power [W]
@@ -66,6 +66,10 @@ if f_a/2 < f_D
     disp('WARNING: Sampling freq too small for doppler freq!');
     disp(' ');
 end
+if v < c/(2*f_0*T_f)
+    disp('WARNING: v too big for speed resolution! Use longer sweep time.');
+    disp(' ');
+end
 %% simulation data
 
 % transmitted signal
@@ -98,7 +102,10 @@ q = c_r * P_s * exp(1i*2*pi*(discrete_int(f_sfcw - f_D, 1/f_a, 1, S)...
     - [discrete_int(f_sfcw, 1/f_a, f_a*(T-tau), S) discrete_int(f_sfcw, 1/f_a, 1, f_a*(T-tau))]));...
     % Filled time delayed vector with truncated values at the beginning to
     % simulate periodicity
-
+% 
+% q = c_r * P_s * exp(1i*2*pi*(discrete_int(f_sfcw, tau, floor(f_a*(T-tau)), S)...
+%     - f_D*t));
+    
 % add noise  
 %q = awgn(q, 60);
 %% Plot
@@ -123,11 +130,11 @@ subplot(3,1,3);
 [q_fft, f_D_est] = spektrum(q, 4096, f_a);
 
 % SFCW frequency plot
-% figure(fig+1);
-% plot(t, f_sfcw);
-% title('SFCW Frequency');
-% xlabel('t/s');
-% ylabel('f/Hz');
+figure(fig+1);
+plot(t, f_sfcw);
+title('SFCW Frequency');
+xlabel('t/s');
+ylabel('f/Hz');
 
 %% Estimation
 
