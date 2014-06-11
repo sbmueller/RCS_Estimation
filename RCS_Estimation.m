e61369447f33bc727c4d65e8d5a6fa7b9aa11077
Sebastian Müller <senpo@posteo.de>
2014-06-11 12:32:44 +0200

    Code cleanup

--- RCS_Estimation.m
+++ RCS_Estimation.m
@@ -1,11 +1,8 @@
 %% Simulation SFCW Radar RCS estimation
 % Bachelor thesis by Sebastian Müller
 
-% clear all variables
-clear all
-
-% clear console
-clc;
+clear all % clear all variables
+clc; % clear console
 
 disp('**** SIMULATION SFCW RADAR ****')
 disp(' ')
@@ -18,14 +15,13 @@ c = 299792458; % speed of light [m/s]
 
 % object constants
 
-R = 10;     % distance radar - target [m]
+R = 80;     % distance radar - target [m]
 v = 0;      % speed of target [m/s] (v>0 -> moves towards receiver)
 sigma = 5;  % radar cross section [m^2]
 
 % radar constants
 
 f_a = 1e6; % sampling frequency [Hz]
-
 f_0 = 24.125e9;   % center frequency [Hz]
 B = 50e6;     % sweep frequency [Hz]
 T_f = 10e-3;  % sweep time per flank [s]
@@ -33,23 +29,27 @@ n = 64;      % frequency steps per flank [1]
 N = 1;      % measuring intervals [1]
 
 P_s = 100;  % transmission power [W]
-G_T = 100;  % transmitting antenna gain [1]
-G_R = 100;  % receiving antenna gain [1]
+G_T = 20;  % transmitting antenna gain [dBi]
+G_R = 20;  % receiving antenna gain [dBi]
 
 N_fft = 4096*128; % FFT Size
 
 %% Calculate remaining data
 
+G_T = db2pow(G_T);  % calculate magnitude of gain
+G_R = db2pow(G_R);  % calculate magnitude of gain
+
 T = N*2*T_f; % calculate measure time
 t = 0:1/f_a:T-1/f_a; % create time vector
 lambda = c / f_0; % wavelength
 
-f_sfcw = SFCW_Freq(t, 2*T_f, f_0, B, n, N); % create sfcw frequency vector
+% f_sfcw = SFCW_Freq(t, 2*T_f, f_0, B, n, N); % create sfcw frequency vector...
+% (not necessary anymore)
 
-tau = 2*R/c;    % time delay
+tau = 2*R/c;    % time radar delay
 f_D = 2*v*f_0/c;    % doppler frequency
 
-f_min = f_0 - B/(2*n);
+f_min = f_0 - B/(2*n); % calculate minimum frequency
 
 %% check values
 
@@ -76,22 +76,12 @@ end
 
 %% Diagnostic debug data
 
-df = B/n;
-dt = T_f/n;
-R_max = c/(2*df)
-dR = c/(2*B)
+df = B/n;   % frequency step
+dt = T_f/n; % time step
+R_max = c/(2*df)    % maximum unambiguous distance
+dR = c/(2*B)    % minimum resolvable distance
 
-phi_bb = baseband_phase(f_min, tau, dt, df, T/N, t); % get phase of baseband signal
-
-% a = angle(q);
-% dp = a(:,100);
-% dp = abs(dp - a(:,105+floor(dt*f_a)));
-% R_est = dp*c/(4*pi*df);
 %% simulation data
-
-% transmitted signal
-
-s = sqrt(P_s) * exp(1i * 2 * pi * discrete_int(f_sfcw, f_a, 0, T));
 
 c_r = (sqrt(G_T)*sqrt(G_R)*lambda*sqrt(sigma))/((4*pi)^(3/2)*R^2); ...
 % attenuation because of radar equation
@@ -101,75 +91,58 @@ if c_r > 1
     c_r = 1;
 end
 
-% received signal
-
-%% Simulate mixing (not recommended, not further developed)
-% r = c_r * sqrt(P_s) * exp(1i * 2 * pi *...
-%     discrete_int(f_sfcw, 1/f_a, 1, floor(f_a*(T-tau))));
-% r = [zeros(1, S - floor(f_a*(T-tau))) r]; % fill received vector with nulls for same length
-% r = r .* exp(1i * 2 * pi * f_D * t); % add phase shift because of doppler
-% 
-% % mixing
-% 
-% q = s.*conj(r);
-
-%% OR use analytical term (recommended)
+% baseband signal (analytical)
 
 % q = c_r * P_s * exp(1i*2*pi*(discrete_int(f_sfcw, f_a, 0, T)...
 %     - [zeros(1, timeToInt(tau, f_a)) discrete_int(f_sfcw, f_a, 0, T-tau)]...
 %     - f_D * t));
-t_dec = decimate(t, 300);
-t_dec  = interp(t_dec, 300, 1);
-t_dec = t_dec(1:length(t));
-q = c_r * P_s * exp(1i*2*pi*(phi_bb - f_D * t_dec));
-    % Filled time delayed vector with truncated values at beginning to
-    % simulate periodizity
 
-    
+phi_bb = baseband_phase(f_min, tau, dt, df, T/N, t); % get phase of baseband signal
+q = c_r * P_s * exp(1i*2*pi*(phi_bb - f_D * t)); % baseband signal
+
 % add noise  
 %q = awgn(q, 60);
 
 %% Estimation
 
-[q_fft, f_D_est, f_R_est] = spektrum(q, N_fft, f_a); % Calculate fft and frequency shift
+[q_fft, f_R_est] = spektrum(q, N_fft, f_a); % Calculate fft and frequency shift
 
 % Calculate estimated v
 
-v_est = c/2 * -f_D_est/f_0; % use negative f_D because we are measuring '-f_D' in analytical term
+v_est = 0 % (not implemented yet)
 
 % Calculate estimated R
+% (we ware still assuming v = 0)
 
 kappa = f_R_est; % calculate spatial frequency (still buggy)
 R_est = c/(2*B)*kappa; % estimate R
 
-
-
 %% Plot
 
-%plot abs
-fig = figure(1);
-subplot(3,1,1);
-plot(t, abs(q));
-title('baseband signal abs');
-xlabel('t/s');
-ylabel('Ampl.');
-
-% plot phase
-subplot(3,1,2);
-plot(t, angle(q));
-title('baseband signal phase');
-xlabel('t/s');
-ylabel('Phase/rad');
-subplot(3,1,3);
-
-% FFT Plot
-x_fa = 0:f_a/N_fft:f_a-f_a/N_fft;
-plot(x_fa-f_a/2, q_fft, 'b.-')
-%axis([-fn fn 0 (max(y)-min(y))/4*1.1])
-title('FFT')
-ylabel('Amplitude')
-xlabel(['Auflösung: ',num2str(df),' Hz Frequenz in Hz'])
-grid
+% %plot abs
+% fig = figure(1);
+% subplot(3,1,1);
+% plot(t, abs(q));
+% title('baseband signal abs');
+% xlabel('t/s');
+% ylabel('Ampl.');
+% 
+% % plot phase
+% subplot(3,1,2);
+% plot(t, angle(q));
+% title('baseband signal phase');
+% xlabel('t/s');
+% ylabel('Phase/rad');
+% subplot(3,1,3);
+% 
+% % FFT Plot
+% x_fa = 0:f_a/N_fft:f_a-f_a/N_fft;
+% plot(x_fa-f_a/2, q_fft, 'b.-')
+% %axis([-fn fn 0 (max(y)-min(y))/4*1.1])
+% title('FFT')
+% ylabel('Amplitude')
+% xlabel(['Auflösung: ',num2str(df),' Hz Frequenz in Hz'])
+% grid
 
 % SFCW frequency plot
 % figure(fig+1);
--- Toolbox/spektrum.m
+++ Toolbox/spektrum.m
@@ -1,4 +1,4 @@
-function [y_fft, fD_x, fR_x] = spektrum(y, N, f_a) 
+function [y_fft, fR_x] = spektrum(y, N, f_a) 
 %spektrum generates fft of signal and finds abs of highest peak
 %   Input:
 %       y:      signal vector
@@ -6,8 +6,9 @@ function [y_fft, fD_x, fR_x] = spektrum(y, N, f_a)
 %       f_a:    sampling frequency
 %   Output:
 %       y_fft:  fft vector of signal
-%       fD_x:   (will be removed) Doppler peak
 %       fR_x:   abs of highest peak
+
+% do fft
 
 df = f_a/N;
 fn = f_a/2;
@@ -17,19 +18,13 @@ H = fft(y, N);
 amplH = abs(H);
 amplitudengang = fftshift(amplH/N);
 
-[peak_y, peak_x] = findpeaks(amplitudengang, 'MINPEAKHEIGHT', ...
-    0.5*(max(amplitudengang) - min(amplitudengang)), 'SORTSTR', 'descend', ...
-    'NPEAKS', 6); % Find two highest peaks in fft, f_D is in the middle
+% find peaks
 
-fD_x = round(mean(peak_x)); % Find middle of two peaks
-fD_x = x_fa(:,fD_x)-fn; % Get corresponding frequency of peak
+[peak_y, peak_x] = findpeaks(amplitudengang, 'NPEAKS', 1, 'SORTSTR', 'descend'); ...
+	% finds highest peak in spectrum
 
-[peak_y, peak_x] = findpeaks(amplitudengang, 'NPEAKS', 1, 'SORTSTR', 'descend');
-fR_x = abs(x_fa(:,round(peak_x))-fn); % Get Highest peak freq
+fR_x = abs(x_fa(:,round(peak_x))-fn); % Get Highest peak freqency
 
-y_fft = amplitudengang;
-
-% plot
-
+y_fft = amplitudengang; % assign fft vector
 
 end 
